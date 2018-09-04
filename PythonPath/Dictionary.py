9c0077a64aeefd59e0ea248823840c202fde68b5
piyush97 <me@piyushmehta.com>
2018-09-04 12:30:21 +0530

    showing keys

--- PythonPath/Dictionary.py
+++ PythonPath/Dictionary.py
@@ -19,3 +19,10 @@ mydictionary3 = {
 print(mydictionary1)
 print(mydictionary2)
 print(mydictionary3)
+
+# show key values
+key1 = mydictionary1['name']
+key2 = mydictionary2['name']
+key3 = mydictionary3['name']
+
+print('key1: ',key1,' key2:', key2, ' key3:', key3)
