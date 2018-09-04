9929491391b5ab9f6cdc414bc54b1e4dfffd9684
piyush97 <me@piyushmehta.com>
2018-09-04 12:38:56 +0530

    List

--- PythonPath/Dictionary.py
+++ PythonPath/Dictionary.py
@@ -26,3 +26,13 @@ key2 = mydictionary2['name']
 key3 = mydictionary3['name']
 
 print('key1: ',key1,' key2:', key2, ' key3:', key3)
+
+
+
+# Converting dictionary to list
+
+print('Converting dictionary1 to a list :',[(k,v) for k,v in mydictionary1.items()])
+print('Converting dictionary2 to a list :',[(k,v) for k,v in mydictionary2.items()])
+print('Converting dictionary3 to a list :',[(k,v) for k,v in mydictionary3.items()])
+
+# Printing # of the elements
