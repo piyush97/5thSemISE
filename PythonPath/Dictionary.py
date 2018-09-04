25924b6400b62367c731da976793f222e5d0d47f
piyush97 <me@piyushmehta.com>
2018-09-04 12:25:32 +0530

    Dictionary

--- PythonPath/Dictionary.py
+++ PythonPath/Dictionary.py
@@ -1,13 +1,21 @@
-mydictionary = {
+mydictionary1 = {
     "name": "Archie",
      "identity": "Student",
      "age": 17,
+}
+
+mydictionary2 = {
     "name": "Piyush",
      "identity": "Student",
      "age": 20 ,
+}
+
+mydictionary3 = {
     "name": "hello",
      "identity": "Student",
      "age": 27
 }
 
-print(mydictionary)
+print(mydictionary1)
+print(mydictionary2)
+print(mydictionary3)
