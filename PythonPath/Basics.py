049522fd60f5daab575849e3247741c7a321e9fe
piyush97 <me@piyushmehta.com>
2018-09-17 10:09:31 +0530

    Basics

---
+++ PythonPath/Basics.py
@@ -0,0 +1,12 @@
+atomicElements = {
+'H': 'Hydrogen',
+'He': 'Helium',
+'Au': 'Gold',
+'S': 'Sulphur'
+}
+atomicElements['Li'] = 'Lithium'
+
+# Printing the atomic atomicElements
+
+for i in atomicElements:
+    print("Key is ", i, "Value is", atomicElements[i])
