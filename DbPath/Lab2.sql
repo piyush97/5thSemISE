f57181124cff62c083a97e34489fcfb55d13a6dd
piyush97 <me@piyushmehta.com>
2018-09-17 15:11:36 +0530

    Inserting values

--- DbPath/Lab2.sql
+++ DbPath/Lab2.sql
@@ -4,3 +4,9 @@ create table employee
     name varchar2(20),
     dept_no integer
   );
+
+  insert into employee values(1,'Piyush',1);
+    insert into employee values(2,'prajwal',2);
+      insert into employee values(3,'mohit',3);
+        insert into employee values(4,'Minhaas',4);
+          insert into employee values(5,'Kumar',5);
