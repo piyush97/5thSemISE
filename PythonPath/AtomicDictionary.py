6ecbf7965218748e473007e34e56ee49f999e982
piyush97 <me@piyushmehta.com>
2018-09-17 09:35:31 +0530

    List of Occurances

--- PythonPath/AtomicDictionary.py
+++ PythonPath/AtomicDictionary.py
@@ -1,4 +1,24 @@
-list1 = ['H', 'He', 'Li', 'Be','B', 'C', 'N', 'N', 'N']
+list1 = ['H', 'He', 'Li','He', 'Li', 'Be','B', 'C', 'N', 'N', 'N']
+# List
+
 print("\n list is")
+
+# Printing List
+
 print(list1)
-print("Number of occurance of N is",list1.count("N"))
+
+# Finding Occurances
+
+Occurance1 = list1.count("N")
+Occurance2 = list1.count("He")
+Occurance3 = list1.count("Li")
+
+# Printing Occurances
+print("Number of occurance of N is",Occurance1)
+print("Number of occurance of He is",Occurance2)
+print("Number of occurance of Li is",Occurance3)
+
+# Printing a List of Occurances
+
+print("List of Occurances\n")
+print([Occurance1,Occurance2,Occurance3])
