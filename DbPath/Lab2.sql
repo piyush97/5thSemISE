ace4cdc74ba82a742af20d62359f1c32a8dded7c
piyush97 <me@piyushmehta.com>
2018-09-17 15:13:30 +0530

    Display

--- DbPath/Lab2.sql
+++ DbPath/Lab2.sql
@@ -1,3 +1,5 @@
+-- Creating table
+
 create table employee
   (
     ssn integer primary key,
@@ -5,8 +7,13 @@ create table employee
     dept_no integer
   );
 
-  insert into employee values(1,'Piyush',1);
-    insert into employee values(2,'prajwal',2);
-      insert into employee values(3,'mohit',3);
-        insert into employee values(4,'Minhaas',4);
-          insert into employee values(5,'Kumar',5);
+-- Inserting values
+
+insert into employee values(1,'Piyush',1);
+  insert into employee values(2,'prajwal',2);
+    insert into employee values(3,'mohit',3);
+      insert into employee values(4,'Minhaas',4);
+        insert into employee values(5,'Kumar',5);
+
+-- showing the values
+select * from employee;
