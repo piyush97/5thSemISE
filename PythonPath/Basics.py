624c978888fac993c17c724daf95242d1b154247
piyush97 <me@piyushmehta.com>
2018-09-17 10:16:14 +0530

    Basics

--- PythonPath/Basics.py
+++ PythonPath/Basics.py
@@ -1,12 +1,39 @@
-atomicElements = {
-'H': 'Hydrogen',
-'He': 'Helium',
-'Au': 'Gold',
-'S': 'Sulphur'
-}
-atomicElements['Li'] = 'Lithium'
+def atomicDictionary():
+    atomicElements = {
+    'H': 'Hydrogen',
+    'He': 'Helium',
+    'Au': 'Gold',
+    'S': 'Sulphur'
+    }
+    atomicElements['Li'] = 'Lithium'
 
-# Printing the atomic atomicElements
+    # Printing the atomic atomicElements
+    for i in atomicElements:
+        print("Key is ", i, "Value is", atomicElements[i])
 
-for i in atomicElements:
-    print("Key is ", i, "Value is", atomicElements[i])
+    numb = len(atomicElements)
+    print("Original no of elements",numb)
+    print("Enter the new atomic element")
+    answer = "y"
+
+    # Add elements into the dicitonary by interacting with user
+    while answer == 'y' or answer == 'Y':
+        key = raw_input("Enter the atomic symbol: \t")
+        value = raw_input("Enter the atom Name: \t")
+        atomicElements[key] = value
+        answer = raw_input("Do you want to enter elements Y or N")
+
+    print(atomicElements)
+
+    print('New Number of elements is', len(atomicElements))
+
+    # Search for the element that the user gives
+    search = " "
+    search = raw_input("Enter the key symbol to search for")
+
+    if search in atomicElements:
+        print("Yes element %s exists", %search)
+        print("Its value is %s", %atomicElements[search])
+    else:
+        print("Element not found")
+    
