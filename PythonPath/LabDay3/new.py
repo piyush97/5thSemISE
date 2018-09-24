5ffece0e429da147ce263d7a10f822f6bb8409ae
piyush97 <me@piyushmehta.com>
2018-09-24 10:11:44 +0530

    person

---
+++ PythonPath/LabDay3/new.py
@@ -0,0 +1,29 @@
+class Person:
+    __secretCount = 0
+    personCount = 0
+    # Params __init__(self,...var)
+    def __init__(self, name, age):
+
+        self.name = name;
+        self.age = age;
+        Person.personCount = Person.personCount + 1
+        self.__secretCount = self.__secretCount + 1
+
+    def display(self):
+        print("\n Name of person is", self.name);
+        print("\n age of person is", self.age);
+        print("PersonCount is ",self.personCount);
+        print("__secretCount is ",self.__secretCount);
+
+p1 = Person("hello", 22)
+p2 = Person("world", 44)
+# Printing after deleting
+p1.display()
+
+print("Displays personCount coz it's public")
+print("PersonCount", Person.personCount)
+print("__secretCount is", Person.__secretCount)
+
+# Throws error
+
+print("\nName of person after deleting is", p1.name)
