579f989097807264cfd6b0beff936c5166f5bc0f
piyush97 <me@piyushmehta.com>
2018-09-24 10:29:55 +0530

    Indentation

--- PythonPath/LabDay3/app.py
+++ PythonPath/LabDay3/app.py
@@ -2,17 +2,18 @@ class Person:
     # Params __init__(self,...var)
     def __init__(p2, name, age):
 
-        p2.name = name;
-        p2.age = age;
+        p2.name = name
+        p2.age = age
+
 
 p1 = Person("suppandi", 14)
 p2 = Person("Ramu", 12)
 
-print("\n Name of person #1 is", p1.name);
-print("\n age of person #1 is", p1.age);
+print("\n Name of person #1 is", p1.name)
+print("\n age of person #1 is", p1.age)
 
-print("\n Name of person #2 is", p2.name);
-print("\n age of person #2 is", p2.age);
+print("\n Name of person #2 is", p2.name)
+print("\n age of person #2 is", p2.age)
 
 p2.age = 10
 
