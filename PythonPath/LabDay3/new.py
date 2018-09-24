cbe0e199b36e59b63ac75893e1acb31137511aa1
piyush97 <me@piyushmehta.com>
2018-09-24 10:15:04 +0530

    indentations

--- PythonPath/LabDay3/new.py
+++ PythonPath/LabDay3/new.py
@@ -1,19 +1,19 @@
 class Person:
-    __secretCount = 0
-    personCount = 0
-    # Params __init__(self,...var)
-    def __init__(self, name, age):
+	__secretCount = 0
+	personCount = 0
+	# Params __init__(self,...var)
+	def __init__(self, name, age):
 
-        self.name = name;
-        self.age = age;
-        Person.personCount = Person.personCount + 1
-        self.__secretCount = self.__secretCount + 1
+		self.name = name;
+		self.age = age;
+		Person.personCount = Person.personCount + 1
+		self.__secretCount = self.__secretCount + 1
 
-    def display(self):
-        print("\n Name of person is", self.name);
-        print("\n age of person is", self.age);
-        print("PersonCount is ",self.personCount);
-        print("__secretCount is ",self.__secretCount);
+	def display(self):
+		print("\n Name of person is", self.name);
+		print("\n age of person is", self.age);
+		print("PersonCount is ",self.personCount);
+		print("__secretCount is ",self.__secretCount);
 
 p1 = Person("hello", 22)
 p2 = Person("world", 44)
