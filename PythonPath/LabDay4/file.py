4b20e6b18024b1ad2797ad40463f9edfae2ab381
piyush97 <me@piyushmehta.com>
2018-09-25 12:25:46 +0530

    file

--- PythonPath/LabDay4/file.py
+++ PythonPath/LabDay4/file.py
@@ -5,3 +5,11 @@ for line in escape:
     print (line)
     myline = line.split()
     print (myline)
+
+    for word in myline:
+        w = wordDic.get(word, 0)
+        print("The word %s occurs in dictionary %d time", word,w )
+        wordDic[word] = w + 1
+        print("after adding the new word read %s", word,wordDic[word])
+        print("the word %s occurs %d times in dictionar")
+print("\n", wordDic)
