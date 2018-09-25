75521f05d58d548589e84b288f4790fb63073823
piyush97 <me@piyushmehta.com>
2018-09-25 12:33:30 +0530

    btf

--- PythonPath/LabDay4/file.py
+++ PythonPath/LabDay4/file.py
@@ -8,14 +8,15 @@ for line in escape:
 
     for word in myline:
         w = wordDic.get(word, 0)
-        print("The word %s occurs in dictionary %d time" %(word, w))
+        print("The word %s occurs in dictionary %d time" % (word, w))
         wordDic[word] = w + 1
-        print("after adding the new word read %s" %word)
-        print("the word %s occurs %d times in dictionar" %(word, wordDic[word]))
+        print("after adding the new word read %s" % word)
+        print("the word %s occurs %d times in dictionar" %
+              (word, wordDic[word]))
 print("\n", wordDic)
 
 search = input("enter search string")
 if search in wordDic:
-    print("\nFound word %s %d times" %(search, wordDic[search]))
+    print("\nFound word %s %d times" % (search, wordDic[search]))
 else:
     print("\nsorry not Found")
