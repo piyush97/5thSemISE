6971dbaaca8fa795dbf54fd2eb1f7e1e6e599626
piyush97 <me@piyushmehta.com>
2018-09-25 11:23:12 +0530

    Occurances

--- PythonPath/LabDay4/app.py
+++ PythonPath/LabDay4/app.py
@@ -1,3 +1,12 @@
-data = {
+fname = input("Enter file name: ")
+word=input("Enter word to be searched:")
+k = 0
 
-}
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            if(i==word):
+                k=k+1
+print("Occurrences of the word:")
+print(k)
