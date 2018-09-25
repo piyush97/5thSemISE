0a8c180aa700bf99716da65d08afc93cce2ba504
piyush97 <me@piyushmehta.com>
2018-09-25 12:30:51 +0530

    fix

--- PythonPath/LabDay4/file.py
+++ PythonPath/LabDay4/file.py
@@ -8,8 +8,8 @@ for line in escape:
 
     for word in myline:
         w = wordDic.get(word, 0)
-        print("The word %s occurs in dictionary %d time", word,w )
+        print("The word %s occurs in dictionary %d time" %(word, w))
         wordDic[word] = w + 1
-        print("after adding the new word read %s", word,wordDic[word])
-        print("the word %s occurs %d times in dictionar")
+        print("after adding the new word read %s" %word)
+        print("the word %s occurs %d times in dictionar" %(word, wordDic[word]))
 print("\n", wordDic)
