34d5574e28adc321cf8f138cd35b33486fee0da9
piyush97 <me@piyushmehta.com>
2018-10-01 09:52:24 +0530

    Lists

--- PythonPath/Misc/ReadElements.py
+++ PythonPath/Misc/ReadElements.py
@@ -2,3 +2,13 @@
 # functions). Use one-line comprehensions of create a new list of even numbers. Create another list
 # reversing the elements.
 list = []
+# List has the following elements
+while(1):
+
+    list.append(int(input("Enter the elements for list")))
+    num = int(input("for exit press 2"))
+    if (num == 2):
+        print(list)
+        exit(0)
+
+print(list)
