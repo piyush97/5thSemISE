cd4ed5ec1a177f2cdda0e1a605b551a7b58fae86
piyush97 <me@piyushmehta.com>
2018-10-01 10:03:14 +0530

    Even

--- PythonPath/Misc/ReadElements.py
+++ PythonPath/Misc/ReadElements.py
@@ -1,14 +1,19 @@
 #  Read a list of elements. Create a new list having all the elements minus the duplicates (Use
 # functions). Use one-line comprehensions of create a new list of even numbers. Create another list
 # reversing the elements.
-list = []
-# List has the following elements
-while(1):
+a = [10,20,30,20,4,5,6,7,8,310,50,60,40,80,50,40]
 
-    list.append(int(input("Enter the elements for list")))
-    num = int(input("for exit press 2"))
-    if (num == 2):
-        print(list)
-        exit(0)
+dup_items = set()
+uniq_items = []
+for x in a:
+    if x not in dup_items:
+        uniq_items.append(x)
+        dup_items.add(x)
 
-print(list)
+print(uniq_items)
+
+# Printing even numbers only
+
+for x in a:
+    if x % 2 == 0:
+        print(x)
