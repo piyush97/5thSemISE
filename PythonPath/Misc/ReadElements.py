e14afd81561abdbc40651b21c9a9bae540759624
piyush97 <me@piyushmehta.com>
2018-10-01 09:41:14 +0530

    Lists

---
+++ PythonPath/Misc/ReadElements.py
@@ -0,0 +1,4 @@
+#  Read a list of elements. Create a new list having all the elements minus the duplicates (Use
+# functions). Use one-line comprehensions of create a new list of even numbers. Create another list
+# reversing the elements.
+list = []
