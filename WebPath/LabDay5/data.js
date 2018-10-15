cd43d833e866bbb05ba06766b8fb48e9a4e17372
piyush97 <me@piyushmehta.com>
2018-10-15 09:43:37 +0530

    data

---
+++ WebPath/LabDay5/data.js
@@ -0,0 +1,19 @@
+module.exports = [{
+    model: 'Hyundai',
+    name: 'Santro',
+    price: 20000,
+    year: 2010
+  },
+  {
+    model: 'Honda',
+    name: 'Amaze',
+    price: 40000,
+    year: 2012
+  },
+  {
+    model: 'Toyota',
+    name: 'Innova',
+    price: 60000,
+    year: 2014
+  },
+]
