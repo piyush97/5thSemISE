9fd0fb73ffb8839a31927815354eac96e06676b7
piyush97 <me@piyushmehta.com>
2018-10-22 10:05:29 +0530

    sentence reverse using vowel count
    Signed-off-by: piyush97 <me@piyushmehta.com>

--- PythonPath/LabDay6/SentenceReverser.py
+++ PythonPath/LabDay6/SentenceReverser.py
@@ -12,13 +12,25 @@ class SentenceReverser:
     def reverseSentence(self):
         self.reverse = ''.join(reversed(self.sentence.split()))
 
+    def getVowelCount(self):
+        for s in self.sentence:
+            if s in self.vowels:
+                self.vowelCount += 1
+            return self.vowelCount
+
     def getReverse(self):
         return self.reverse
 
+        items = []
 
-items = []
 
 for i in range(3):
     sentence = input('Enter a phrase:')
     reverser = SentenceReverser(sentence.strip())
     print(reverser.getReverse())
+
+sortedItems = sorted(items, key=lambda item: item, .getVowelCount(),)
+print(sortedItems[0].getReverse())
+print("Sorted on vowel count (Descending)\n")
+
+for i in range(len(sortedItems)):
